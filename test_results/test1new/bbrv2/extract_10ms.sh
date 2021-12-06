ba6362ecab95deeae598c1f93f6bdbcda23208e7
Nas Sultany <nasratullah.sultany@gmail.com>
2021-12-06 04:35:33 +0000

    Add script for extracting data as well as the extracted data.

---
+++ test_results/test1new/bbrv2/extract_10ms.sh
@@ -0,0 +1,6 @@
+for i in $(seq 1 27); do
+	tmp="test1_bbr2_10ms_"
+	fx="_.log"
+	filename="${tmp}${i}${fx}" 
+	cat ${filename} | grep -Po '[0-9.]*(?= Mbits/sec)' | tail -2 | head -1 >> test1_bbr2_10ms.dat
+done
